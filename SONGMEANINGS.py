11e76b754b1a4c7ddd4aab05d76ea17e642d2634
miishin <miraisahara@miraimacbookair.campus.sjcny.edu>
2015-07-22 11:38:51 -0400

    updates

--- SONGMEANINGS.py
+++ SONGMEANINGS.py
@@ -2,7 +2,7 @@ import urllib2
 import json
 
 
-def asdf(artist):
+def getPlaylist(artist):
     songids = []
     artists = []
     y = urllib2.urlopen("http://developer.echonest.com/api/v4/playlist/static?api_key=KK2PINTHA7KOVR4DK&artist=" + artist + "&format=json&results=20&type=artist-radio&sort=artist_hotttnesss-desc")
@@ -15,7 +15,7 @@ def getSongs(aD):
     songs = []
     for diction in aD:
         songs.append(diction["title"])
-    return songs
+    return songs 
 
 
 def getIDs(aD):
@@ -29,5 +29,19 @@ def getArtists(aD):
     for diction in aD:
         artists.append(diction["artist_name"])
     return artists
+
+def getLinks(aD):
+    links = []
+    link = "http://http://songmeanings.com/songs/view/"
+    for diction in aD:
+        links.append(link + diction["id"] + "/")
+    return links
+
+def getInfos(songs, artists, links):
+    someList = []
+    for x in xrange(len(songs)):
+        someList.append(songs[x] + "\n" + artists[x] + "\n" + links[x] + "\n")
+    return someList
+
 #api key
 #KK2PINTHA7KOVR4DK 
